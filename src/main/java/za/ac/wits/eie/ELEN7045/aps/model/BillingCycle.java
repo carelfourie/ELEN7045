2a9377b308561f4259126d26ffa91d1f96187d6c
carel_fourie <carel.fourie@gmail.com>
2014-06-05 17:29:23 +0200

    Progress...

--- src/main/java/za/ac/wits/eie/ELEN7045/aps/model/BillingCycle.java
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/model/BillingCycle.java
@@ -1,11 +1,15 @@
 package za.ac.wits.eie.ELEN7045.aps.model;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 import za.ac.wits.eie.ELEN7045.aps.model.base.BaseDomainEntity;
 
 @Entity
 public class BillingCycle extends BaseDomainEntity<Long> {
+    
+    @OneToOne
+    private Company company;
 
     public BillingCycle() {
         super();
--- src/main/java/za/ac/wits/eie/ELEN7045/aps/model/Company.java
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/model/Company.java
@@ -1,11 +1,15 @@
 package za.ac.wits.eie.ELEN7045.aps.model;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 import za.ac.wits.eie.ELEN7045.aps.model.base.BaseDomainEntity;
 
 @Entity
 public class Company extends BaseDomainEntity<Long> {
+    
+    @OneToOne
+    private BillingCycle billingCycle;
 
     public Company() {
         super();
