3cf600278a253aa93c8aad25557c309590101523
Thierry Mbelli <mbellimbah@hotmail.com>
2014-06-15 14:27:04 +0200

    change ejb remote to local

--- src/main/java/za/ac/wits/eie/ELEN7045/aps/service/APSUserAccountCreationService.java
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/service/APSUserAccountCreationService.java
@@ -1,10 +1,10 @@
 package za.ac.wits.eie.ELEN7045.aps.service;
 
-import javax.ejb.Remote;
+import javax.ejb.Local;
 
 import za.ac.wits.eie.ELEN7045.aps.model.APSUser;
 
-@Remote
+@Local
 public interface APSUserAccountCreationService {
 
 	/**
