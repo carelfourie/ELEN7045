c8fe14ad2307941755a15645bf9fbb69431cea1f
carel_fourie <carel.fourie@gmail.com>
2014-06-21 12:44:01 +0200

    Progress...

--- src/main/java/za/ac/wits/eie/ELEN7045/aps/controller/AccountInfo.java
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/controller/AccountInfo.java
@@ -3,26 +3,32 @@ package za.ac.wits.eie.ELEN7045.aps.controller;
 public class AccountInfo {
 
 	private String password1;
-    private String password2;
-    private String username;
+    
+	private String password2;
+    
+	private String username;
+    
 	public String getPassword1() {
 		return password1;
 	}
+	
 	public String getPassword2() {
 		return password2;
 	}
+	
 	public String getUsername() {
 		return username;
 	}
+	
 	public void setPassword1(String password1) {
 		this.password1 = password1;
 	}
+	
 	public void setPassword2(String password2) {
 		this.password2 = password2;
 	}
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
-    
-    
 }
