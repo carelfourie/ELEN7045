6b6be834c525488718c485d10e83a5ac5894dae5
carel_fourie <carel.fourie@gmail.com>
2014-06-21 13:40:32 +0200

    Progress...

--- src/main/java/za/ac/wits/eie/ELEN7045/aps/service/LoginService.java
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/service/LoginService.java
@@ -6,9 +6,6 @@ import java.util.Map;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceContextType;
 
 import org.hibernate.criterion.Restrictions;
 
@@ -20,9 +17,6 @@ import za.ac.wits.eie.ELEN7045.aps.service.exception.APSException;
 
 @Stateless
 public class LoginService extends BaseService {
-
-    @PersistenceContext(type = PersistenceContextType.TRANSACTION, unitName = "primary")
-    protected EntityManager entityManager;
 
     @Inject
     private FindByCriteriaRepository findByCriteriaRepo;
