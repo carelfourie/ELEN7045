a6ecc07e7716d6ac1115541f34974751c6224b69
carel_fourie <carel.fourie@gmail.com>
2014-06-23 21:44:22 +0200

    Progress...

--- src/main/java/za/ac/wits/eie/ELEN7045/aps/service/base/BaseService.java
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/service/base/BaseService.java
@@ -1,12 +1,32 @@
 package za.ac.wits.eie.ELEN7045.aps.service.base;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.jboss.logging.Logger;
+
+import za.ac.wits.eie.ELEN7045.aps.service.exception.APSException;
 
 public abstract class BaseService {
     
     @Inject
     protected Logger log;
+    
+    protected void listCheck(List<?> list,  Map<String, String> msgVal) throws APSException {
+        if (list == null) {
+            throw new APSException("List cannot be null.");
+        }
+       
+        String[] keys = msgVal.keySet().toArray(new String[msgVal.keySet().size()]);
+        
+        if (list.size() < 1) {
+            throw new APSException(String.format(keys[0], msgVal.get(keys[0])));
+        }
 
+        if (list.size() > 1) {
+            throw new APSException(String.format(keys[1], msgVal.get(keys[1])));
+        }
+    }
 }
