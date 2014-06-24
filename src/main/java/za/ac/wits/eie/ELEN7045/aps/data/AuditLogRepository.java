698b74dd83534725f62e8c822a5fe1e329c10882
carel_fourie <carel.fourie@gmail.com>
2014-06-24 22:14:41 +0200

    Progress...

--- src/main/java/za/ac/wits/eie/ELEN7045/aps/data/AuditLogRepository.java
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/data/AuditLogRepository.java
@@ -3,9 +3,7 @@ package za.ac.wits.eie.ELEN7045.aps.data;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
@@ -24,13 +22,4 @@ public class AuditLogRepository  extends BaseRepository {
 		
 		return users;
 	}
-	
-//    public ScrapingEventAuditLog findByMember(String name) {
-//        CriteriaBuilder cb = em.getCriteriaBuilder();
-//        CriteriaQuery<ScrapingEventAuditLog> criteria = cb.createQuery(ScrapingEventAuditLog.class);
-//        Root<ScrapingEventAuditLog> log = criteria.from(ScrapingEventAuditLog.class);
-//        criteria.select(log).where(cb.equal(log.get("name"), name)).orderby();
-//        return em.createQuery(criteria).getSingleResult();
-//    }
-    
 }
