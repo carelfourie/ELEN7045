fdac3782fd9639a98f3a6bbda1a065a22908cd1d
carel_fourie <carel.fourie@gmail.com>
2014-06-28 14:56:36 +0200

    Progress...

---
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/resource/Producer.java
@@ -0,0 +1,29 @@
+package za.ac.wits.eie.ELEN7045.aps.resource;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+
+import org.jboss.logging.Logger;
+
+public class Producer {
+
+    @Produces
+    @PersistenceContext(type = PersistenceContextType.TRANSACTION, unitName = "primary")
+    protected EntityManager em;
+    
+    @Produces
+    @RequestScoped
+    public FacesContext produceFacesContext() {
+        return FacesContext.getCurrentInstance();
+    }
+
+    @Produces
+    public Logger produceLog(InjectionPoint injectionPoint) {
+        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+    }
+}
