f7ff3c2f705ee12470c09bf8fadc3bf3c4911d55
carel_fourie <carel.fourie@gmail.com>
2014-06-28 15:14:44 +0200

    Progress...

--- src/main/java/za/ac/wits/eie/ELEN7045/aps/model/ScrapingEventAuditLog.java
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/model/ScrapingEventAuditLog.java
@@ -10,55 +10,56 @@ import za.ac.wits.eie.ELEN7045.aps.model.base.BaseDomainEntity;
 
 @Entity
 public class ScrapingEventAuditLog extends BaseDomainEntity<Long> {
-	
-	  @OneToOne
-	  private APSUser apsUser;
-	  
-	  @OneToOne
-	  private Company company;
-	  
-	  @OneToOne
-	  private ReturnCode returnCode;
-	  
-	  @Column(name = "scrapeDate")
-	  private Date scrapeDate;
-	  
-	  
-	  
-	public ScrapingEventAuditLog() {
-		super();
-	}
-	public ScrapingEventAuditLog(Long id) {
-		super(id);
-	}
-	public APSUser getApsUser() {
-		return apsUser;
-	}
-	public Company getCompany() {
-		return company;
-	}
-	public ReturnCode getReturnCode() {
-		return returnCode;
-	}
-	public Date getScrapeDate() {
-		return scrapeDate;
-	}
-	
-	
-	public void setApsUser(APSUser apsUser) {
-		this.apsUser = apsUser;
-	}
-	public void setCompany(Company company) {
-		this.company = company;
-	}
-	public void setReturnCode(ReturnCode returnCode) {
-		this.returnCode = returnCode;
-	}
-	public void setScrapeDate(Date scrapeDate) {
-		this.scrapeDate = scrapeDate;
-	}
-	  
-	
-	
-	 
+
+    @OneToOne
+    private APSUser apsUser;
+
+    @OneToOne
+    private Company company;
+
+    @OneToOne
+    private ReturnCode returnCode;
+
+    @Column(name = "scrapeDate")
+    private Date scrapeDate;
+
+    public ScrapingEventAuditLog() {
+        super();
+    }
+
+    public ScrapingEventAuditLog(Long id) {
+        super(id);
+    }
+
+    public APSUser getApsUser() {
+        return apsUser;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public ReturnCode getReturnCode() {
+        return returnCode;
+    }
+
+    public Date getScrapeDate() {
+        return scrapeDate;
+    }
+
+    public void setApsUser(APSUser apsUser) {
+        this.apsUser = apsUser;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public void setReturnCode(ReturnCode returnCode) {
+        this.returnCode = returnCode;
+    }
+
+    public void setScrapeDate(Date scrapeDate) {
+        this.scrapeDate = scrapeDate;
+    }
 }
