966b6120d65e3075995fe450e3f1472927a55bc5
carel_fourie <carel.fourie@gmail.com>
2014-06-28 15:23:22 +0200

    Progress...

--- src/main/java/za/ac/wits/eie/ELEN7045/aps/controller/ScraperInfo.java
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/controller/ScraperInfo.java
@@ -7,13 +7,14 @@ import za.ac.wits.eie.ELEN7045.aps.model.APSUser;
 import za.ac.wits.eie.ELEN7045.aps.model.base.BaseDomainEntity;
 
 @Entity
-public class ScraperInfo  extends BaseDomainEntity<Long>{
+public class ScraperInfo extends BaseDomainEntity<Long> {
 
 	@ManyToOne
 	private APSUser apsUser;
-	private String url;
-	private String xmlResultFile;
 	
+	private String url;
+	
+	private String xmlResultFile;
 
 	public APSUser getApsUser() {
 		return apsUser;
@@ -38,7 +39,4 @@ public class ScraperInfo  extends BaseDomainEntity<Long>{
 	public void setXmlResultFile(String xmlResultFile) {
 		this.xmlResultFile = xmlResultFile;
 	}
-
-	
-
 }
