dfd92126618ed30236f30c4886a60fd0156c0d5b
carel_fourie <carel.fourie@gmail.com>
2014-06-28 15:14:09 +0200

    Progress...

--- src/main/java/za/ac/wits/eie/ELEN7045/aps/model/Status.java
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/model/Status.java
@@ -6,5 +6,12 @@ import za.ac.wits.eie.ELEN7045.aps.model.base.BaseDescriptionDomainEntity;
 
 @Entity
 public class Status extends BaseDescriptionDomainEntity<Long> {
-    
+
+    public Status() {
+        super();
+    }
+
+    public Status(Long id) {
+        super(id);
+    }
 }
