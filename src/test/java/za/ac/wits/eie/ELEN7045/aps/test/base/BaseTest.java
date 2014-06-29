1ea1e125798923236664298cf99af35978a72e69
carel_fourie <carel.fourie@gmail.com>
2014-06-29 15:36:21 +0200

    Progress...

--- src/test/java/za/ac/wits/eie/ELEN7045/aps/test/base/BaseTest.java
+++ src/test/java/za/ac/wits/eie/ELEN7045/aps/test/base/BaseTest.java
@@ -14,7 +14,6 @@ import za.ac.wits.eie.ELEN7045.aps.concurrency.ScrapingSession;
 import za.ac.wits.eie.ELEN7045.aps.dto.ScrapeResultDTO;
 import za.ac.wits.eie.ELEN7045.aps.model.APSUser;
 import za.ac.wits.eie.ELEN7045.aps.resource.Producer;
-import za.ac.wits.eie.ELEN7045.aps.rest.JAXRSActivator;
 
 public abstract class BaseTest {
 
@@ -24,7 +23,7 @@ public abstract class BaseTest {
 		File[] libs = pom.resolve().withTransitivity().asFile();
 
 		return ShrinkWrap.create(WebArchive.class, "jee_aps_app_test.war").addClasses(BaseTest.class).addClass(Producer.class).addPackages(true, ScrapingSession.class.getPackage())
-				.addPackages(true, APSUser.class.getPackage()).addPackages(true, JAXRSActivator.class.getPackage()).addPackages(true, ScrapeResultDTO.class.getPackage())
+				.addPackages(true, APSUser.class.getPackage()).addPackages(true, ScrapeResultDTO.class.getPackage())
 				.addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml").addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml").addAsWebInfResource("test-ds.xml")
 				.addAsLibraries(libs);
 	}
