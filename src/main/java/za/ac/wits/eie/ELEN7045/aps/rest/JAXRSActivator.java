9e6ef2d0fe3573db4138d3a67b3001ac5947e52d
carel_fourie <carel.fourie@gmail.com>
2014-06-29 14:33:39 +0200

    Progress...

--- src/main/java/za/ac/wits/eie/ELEN7045/aps/rest/JAXRSActivator.java
+++
@@ -1,9 +0,0 @@
-package za.ac.wits.eie.ELEN7045.aps.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class JAXRSActivator extends Application {
-
-}
