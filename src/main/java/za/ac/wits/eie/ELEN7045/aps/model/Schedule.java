3336f783abc1679c0d0b63fa7581080fafb00ed9
rockymedalie <Rocksmed@@2>
2014-06-30 23:30:04 +0200

    Schedule

---
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/model/Schedule.java
@@ -0,0 +1,10 @@
+package za.ac.wits.eie.ELEN7045.aps.model;
+
+import javax.persistence.Entity;
+
+import za.ac.wits.eie.ELEN7045.aps.model.base.BaseDomainEntity;
+
+@Entity
+public class Schedule extends BaseDomainEntity<Long> {
+
+}
---
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/service/SchedulerService.java
@@ -0,0 +1,14 @@
+package za.ac.wits.eie.ELEN7045.aps.service;
+
+import javax.inject.Inject;
+
+import za.ac.wits.eie.ELEN7045.aps.repository.FindByCriteriaRepository;
+import za.ac.wits.eie.ELEN7045.aps.service.base.BaseService;
+
+public class SchedulerService extends BaseService {
+
+	@Inject
+	private FindByCriteriaRepository findByCriteriaRepo;
+	
+	
+}
