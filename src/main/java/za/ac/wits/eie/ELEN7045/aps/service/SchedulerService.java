b67e6254f1a72ae1397bfd7d3f1bf15716a1a4fb
carel_fourie <carel.fourie@gmail.com>
2014-07-01 22:28:42 +0200

    Progress...

--- src/main/java/za/ac/wits/eie/ELEN7045/aps/service/SchedulerService.java
+++ src/main/java/za/ac/wits/eie/ELEN7045/aps/service/SchedulerService.java
@@ -1,15 +1,13 @@
 package za.ac.wits.eie.ELEN7045.aps.service;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 
-import za.ac.wits.eie.ELEN7045.aps.repository.FindByCriteriaRepository;
 import za.ac.wits.eie.ELEN7045.aps.service.base.BaseService;
 
 @Stateless
 public class SchedulerService extends BaseService {
 
-	@Inject
-	private FindByCriteriaRepository findByCriteriaRepo;
+	//@Inject
+	//private FindByCriteriaRepository findByCriteriaRepo;
 
 }
